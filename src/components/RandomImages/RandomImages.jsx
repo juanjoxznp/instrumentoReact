64bbd30294894541be137efd52efa69649991ca5
Edwin Rozo - edwindev <erozog125@gmail.com>
2024-06-12 16:47:22 -0500

    Instrumento de evaluación b

---
+++ src/components/RandomImages/RandomImages.jsx
@@ -0,0 +1,14 @@
+let pictures = [
+  '../../../assets/Images/01.jpg',
+  '../../../assets/Images/02.jpeg',
+  '../../../assets/Images/03.jpg',
+  '../../../assets/Images/04.jpg',
+  '../../../assets/Images/05.jpg',
+  '../../../assets/Images/06.jpg',
+  '../../../assets/Images/07.jpg',
+  '../../../assets/Images/08.jpg',
+  '../../../assets/Images/09.jpg',
+  '../../../assets/Images/10.jpg',
+]
+
+export const RandomImages = () => pictures
