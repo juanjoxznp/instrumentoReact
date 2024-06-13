862d14572e61b59de3c50956692a26c85ddc240a
Juanjoxznp <juanjoxznp@gmail.com>
2024-06-13 09:08:48 -0500

    Importo Link en componente Welcome y redirecciono a Paint

--- src/components/Layouts/Welcome/Welcome.jsx
+++ src/components/Layouts/Welcome/Welcome.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+//Importo Link y cambio el ancla por el link para poder redireccionar desde Welcome a Paint
 
 export const Welcome = ({ ficha }) => {
   return (
     <>
       <h1>instrumento de evaluación</h1>
       <h2>Ficha: {ficha} </h2>
-      <a href='#'>Ingresar</a>
+      <Link to='/paint'>Ingresar</Link>
     </>
   )
 }
