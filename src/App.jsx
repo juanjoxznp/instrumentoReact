65e1e4b59aa99b819f39d53cefd0bd7a84907196
Juanjoxznp <juanjoxznp@gmail.com>
2024-06-13 09:06:35 -0500

    Importacion de Routes y Route y creacion de ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
 import { Paint } from './components/Layouts/Paint/Paint'
 import { Welcome } from './components/Layouts/Welcome/Welcome'
+import { Routes, Route } from 'react-router'
+
+//Importo Routes y Route, luego dentro de la funcion, creo Routes y dentro de ella creo la ruta especifica
+//para cada elemento que necesito
 
 function App() {
   
   return (
     <>
-      <Paint />      
+
+    <Routes>
+
+      <Route path='/paint' element={ <Paint />} />
+
+
+    </Routes>
+
+           
     </>
   )
 }
