500c51eb2e52db946b34037a7641286a074cbd39
Edwin Rozo Gómez <53238539+erozog125@users.noreply.github.com>
2024-06-13 07:58:59 -0500

    Update RandomImages.jsx

--- src/components/RandomImages/RandomImages.jsx
+++ src/components/RandomImages/RandomImages.jsx
@@ -1,14 +1,14 @@
 let pictures = [
-  '../../../assets/Images/01.jpg',
-  '../../../assets/Images/02.jpeg',
-  '../../../assets/Images/03.jpg',
-  '../../../assets/Images/04.jpg',
-  '../../../assets/Images/05.jpg',
-  '../../../assets/Images/06.jpg',
-  '../../../assets/Images/07.jpg',
-  '../../../assets/Images/08.jpg',
-  '../../../assets/Images/09.jpg',
-  '../../../assets/Images/10.jpg',
+  'https://res.cloudinary.com/erpersonalprojects/image/upload/v1718282333/05_jrzksc.jpg',
+  'https://res.cloudinary.com/erpersonalprojects/image/upload/v1718282326/03_zr8wag.jpg',
+  'https://res.cloudinary.com/erpersonalprojects/image/upload/v1718282323/10_pewtqe.jpg',
+  'https://res.cloudinary.com/erpersonalprojects/image/upload/v1718282319/02_bc60g5.jpg',
+  'https://res.cloudinary.com/erpersonalprojects/image/upload/v1718282314/09_tuiuqj.jpg',
+  'https://res.cloudinary.com/erpersonalprojects/image/upload/v1718282311/01_lyqqvq.jpg',
+  'https://res.cloudinary.com/erpersonalprojects/image/upload/v1718282309/08_oaogxq.jpg',
+  'https://res.cloudinary.com/erpersonalprojects/image/upload/v1718282307/04_hhdzdp.jpg',
+  'https://res.cloudinary.com/erpersonalprojects/image/upload/v1718282275/02_tpg6il.jpg',
+  'https://res.cloudinary.com/erpersonalprojects/image/upload/v1674503975/cld-sample-4.jpg',
 ]
 
 export const RandomImages = () => pictures
